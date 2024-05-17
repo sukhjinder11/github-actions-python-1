2358b96ddfb5812ce553ab1073b3738abbfb1e29
sukhjinder11 <sukhjinderec@gmail.com>
2024-05-17 10:42:37 +0530

    tests added

--- functions/hello-world/tests/test_unit_hello_world.py
+++ functions/hello-world/tests/test_unit_hello_world.py
@@ -1,6 +1,17 @@
+import unittest
+# from unittest.mock import mock, patch
+from src import hello_world
 
-from ..src import hello_world
 
-class TestGreetings:
+class TestGreetings(unittest.TestCase):
+    def setUp(self):
+        self.greet = hello_world.Greetings()
 
-    def test_
+    def tearDown(self):
+        self.greet = None
+
+    def test_hello_world(self):
+        self.assertEqual(self.greet.hello_world(), 'hello world')
+
+    def test_goodbye(self):
+        self.assertEqual(self.greet.goodbye(), 'goodbye!')
